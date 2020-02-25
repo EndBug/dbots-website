d88ec8d249c0c18ce1d068d857d4dc11f0359d8b
Federico Grandi <fgrandi30@gmail.com>
2020-02-25 21:31:48 +0100

    Add website with minimal edits

---
+++ src/data/MainSource.js
@@ -0,0 +1,13 @@
+import semver from 'semver';
+import DocsSource from './DocsSource';
+
+const branchBlacklist = new Set(['docs', 'webpack', 'v8']);
+export default new DocsSource({
+  id: 'main',
+  name: 'Main library',
+  global: 'dbots',
+  repo: 'Snazzah/dbots.js',
+  defaultTag: 'v1.3.0',
+  branchFilter: branch => !branchBlacklist.has(branch) && !branch.startsWith('dependabot/'),
+  tagFilter: tag => semver.gt(tag.replace(/^v/, ''), '1.3.0'),
+});
