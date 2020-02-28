6e5655565c2332442edc507704b6ff1b07df84cc
Federico Grandi <fgrandi30@gmail.com>
2020-02-28 21:59:37 +0100

    Update vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,3 @@
-const webpack = require('webpack');
-
 module.exports = {
-  configureWebpack: () => ({
-    plugins: [
-      new webpack.DefinePlugin({
-        GIT_COMMIT_HASH: JSON.stringify(
-          require('child_process')
-            .execSync('git rev-parse HEAD')
-            .toString()
-            .trim()
-        ),
-        PACKAGE_VERSION: JSON.stringify(require('./package').version),
-        BUILT_AT: JSON.stringify(Date.now()),
-      }),
-    ],
-  }),
+  publicPath: 'dbots-website'
 };
