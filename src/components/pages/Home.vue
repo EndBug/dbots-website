1fc0b5aafb31e2b1cfa60c2d4b3b2a629df1d2d9
Federico Grandi <fgrandi30@gmail.com>
2020-02-28 12:17:25 +0100

    Edit home

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -9,13 +9,10 @@
       <div class="info-item">
         <h2>About</h2>
         <p>
-          discord.js is a powerful <a href="https://nodejs.org/">node.js</a> module that allows you to interact with the
-          <a href="https://discordapp.com/developers/docs/intro">Discord API</a> very easily.
-          It takes a much more object-oriented approach than most other JS Discord libraries, making your bot's code significantly tidier and easier to comprehend.
+          dbots is a package that allows you to automatically post your bot stats to multiple bot lists.
         </p>
         <p>
-          Usability, consistency, and performance are key focuses of discord.js, and it also has nearly 100% coverage of the Discord API.
-          It receives new Discord features shortly after they arrive in the API.
+          The module also provides some interfaces for other requests to the list APIs, so that everything can be done with the same package, instead of having to make the requests youself or dealing with multiple packages.
         </p>
       </div>
 
@@ -23,31 +20,19 @@
         <h2>Example</h2>
         <pre><code class="javascript" v-hljs>const Discord = require('discord.js');
 const client = new Discord.Client();
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+const dbots = require('dbots');
+const poster = new dbots.Poster({
+    client, // Client MUST be logged in to be put into a poster
+    apiKeys: {
+        discordbotsgg: '…',
+        topgg: '…',
+        lsterminalink: '…',
+        carbon: '…'
+    },
+    clientLibrary: 'discord.js'
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
-});
-
-client.login('token');</code></pre>
-      </div>
-
-      <div class="info-item">
-        <h2>Why?</h2>
-        <p>
-          <ul>
-            <li>Object-oriented</li>
-            <li>Speedy and efficient</li>
-            <li>Feature-rich</li>
-            <li>Flexible</li>
-            <li>100% Promise-based</li>
-          </ul>
-        </p>
+poster.startInterval(); // starts an interval thats posts to all services every 30 minutes</code></pre>
       </div>
 
       <div class="info-item">
@@ -86,132 +71,136 @@ export default {
 </script>
 
 <style lang="scss">
-  @import '../../styles/theming';
-  @import '../../styles/mq';
+@import "../../styles/theming";
+@import "../../styles/mq";
 
-  #home {
-    text-align: center;
-    background: $color-content-bg;
-    color: $color-content-text;
+#home {
+  text-align: center;
+  background: $color-content-bg;
+  color: $color-content-text;
 
-    h2 {
-      font-size: 2rem;
-      margin: 0;
-      font-weight: 600;
-      letter-spacing: -1px;
+  h2 {
+    font-size: 2rem;
+    margin: 0;
+    font-weight: 600;
+    letter-spacing: -1px;
+  }
+
+  p {
+    font-size: 1rem;
+    line-height: 2rem;
+    text-align: left;
+
+    @include mq($until: tablet) {
+      margin: 0 1rem;
     }
 
-    p {
-      font-size: 1rem;
-      line-height: 2rem;
-      text-align: left;
-
-      @include mq($until: tablet) {
-        margin: 0 1rem;
-      }
-
-      & > a {
-        font-weight: bold;
-      }
-
-      &.center {
-        text-align: center;
-      }
+    & > a {
+      font-weight: bold;
     }
 
-    #jumbotron {
-      flex: 0 0 100%;
-      padding: 80px 16px;
-      background-color: $color-primary;
-      background-image: linear-gradient(top, $color-primary, darken($color-primary, 5%));
-
-      code {
-        display: inline-block;
-        padding: 16px;
-        border-radius: 5px;
-        background: #f5f5f5;
-        color: $color-content-text;
-        font-family: $font-monospace;
-      }
-    }
-
-    #logo {
-      display: block;
-      margin: 16px auto;
-      width: 95%;
-      max-width: 700px;
-      overflow: visible;
-      filter: drop-shadow(0 3px 4px #333);
-    }
-
-    #info {
-      display: flex;
-      flex-flow: row wrap;
-      margin: 64px auto 0 auto;
-      max-width: 1120px;
-
-      @include mq($until: tablet) {
-        margin: 32px auto 0 auto;
-      }
-    }
-
-    .info-item {
-      flex: 1 1 45%;
-      padding: 24px;
-
-      ul {
-        display: inline-block;
-        margin: 0 auto;
-        text-align: left;
-      }
-
-      pre {
-        padding: 8px;
-        border-radius: 4px;
-        text-align: left;
-        font-size: 0.875rem;
-        background: #F0F0F0;
-      }
-    }
-
-    .full-info-item {
-      flex: 1 0 100%;
-      margin: 48px 0;
-    }
-
-    .big-ass-btn {
-      display: inline-block;
-      padding: 12px 10px;
-      border-radius: 4px;
-      background: $color-primary;
-      color: white;
-      font-size: 1.1rem;
-
-      &:hover {
-        background: lighten($color-primary, 10%);
-      }
-
-      &:not(:first-of-type) {
-        margin-left: 8px;
-      }
-
-      &.discord {
-        background-color: #7289da;
-
-        &:hover {
-          background: lighten(#7289da, 10%);
-        }
-      }
+    &.center {
+      text-align: center;
     }
   }
 
-  #app.dark #home {
-    background: $color-content-bg-dark;
-    color: $color-content-text-dark;
+  #jumbotron {
+    flex: 0 0 100%;
+    padding: 80px 16px;
+    background-color: $color-primary;
+    background-image: linear-gradient(
+      top,
+      $color-primary,
+      darken($color-primary, 5%)
+    );
+
+    code {
+      display: inline-block;
+      padding: 16px;
+      border-radius: 5px;
+      background: #f5f5f5;
+      color: $color-content-text;
+      font-family: $font-monospace;
+    }
+  }
+
+  #logo {
+    display: block;
+    margin: 16px auto;
+    width: 95%;
+    max-width: 700px;
+    overflow: visible;
+    filter: drop-shadow(0 3px 4px #333);
+  }
+
+  #info {
+    display: flex;
+    flex-flow: row wrap;
+    margin: 64px auto 0 auto;
+    max-width: 1120px;
+
+    @include mq($until: tablet) {
+      margin: 32px auto 0 auto;
+    }
+  }
+
+  .info-item {
+    flex: 1 1 45%;
+    padding: 24px;
+
+    ul {
+      display: inline-block;
+      margin: 0 auto;
+      text-align: left;
+    }
 
     pre {
-      background: lighten($color-content-bg-dark, 4%);
-      color: darken($color-content-text-dark, 20%);
+      padding: 8px;
+      border-radius: 4px;
+      text-align: left;
+      font-size: 0.875rem;
+      background: #f0f0f0;
     }
   }
+
+  .full-info-item {
+    flex: 1 0 100%;
+    margin: 48px 0;
+  }
+
+  .big-ass-btn {
+    display: inline-block;
+    padding: 12px 10px;
+    border-radius: 4px;
+    background: $color-primary;
+    color: white;
+    font-size: 1.1rem;
+
+    &:hover {
+      background: lighten($color-primary, 10%);
+    }
+
+    &:not(:first-of-type) {
+      margin-left: 8px;
+    }
+
+    &.discord {
+      background-color: #7289da;
+
+      &:hover {
+        background: lighten(#7289da, 10%);
+      }
+    }
+  }
+}
+
+#app.dark #home {
+  background: $color-content-bg-dark;
+  color: $color-content-text-dark;
+
+  pre {
+    background: lighten($color-content-bg-dark, 4%);
+    color: darken($color-content-text-dark, 20%);
+  }
+}
 </style>
